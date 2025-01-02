0dc47042d962f9579ad50a39fdc3605abe7d1e9f
visxl <measvathanakvisal@gmail.com>
2025-01-02 16:17:00 +0700

    fix images not showing:4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
       failOnError: false, // Ignore errors during prerendering
     },
   },
-  // app: {
-  //   baseURL: '/portfolio/', // GitHub repository name
-  // },
+  app: {
+    baseURL: '/portfolio/', // GitHub repository name
+  },
 })
