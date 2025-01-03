43b60131c9727f3b078db23b803c31eb5c98dab9
visxl <measvathanakvisal@gmail.com>
2025-01-03 16:35:11 +0700

    initial commit

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,10 @@
 
     <main class="gap-4 px-4 mt-4 ">
         <div class="relative h-[850px] w-full bg-gray-900 rounded-xl">
-            <img src="/images/photo-1519681393784-d120267933ba.jpg" class="absolute inset-0 object-cover w-full h-full shadow-md opacity-50 rounded-xl grayscale"/>
+            <!-- <img src="/images/dreamy-landscape-4k-wv-1920x1080.jpg" class="absolute inset-0 object-cover w-full h-full shadow-md opacity-50 rounded-xl grayscale"/> -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="absolute inset-0 object-cover w-full h-full shadow-md opacity-50 rounded-xl grayscale">
+                <image href="/home/visal/Desktop/Workspace/Nuxtjs/my-portfolio/portfolio/public/images/dreamy-landscape-4k-wv-1920x1080.jpg" />
+            </svg>
             <span class="absolute inset-0 flex items-center justify-center text-5xl font-bold text-white md:text-7xl">
                 <span class="text-center">Hello, I'm Meas Vathanakvisal</span> 
             </span>
